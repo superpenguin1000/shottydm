95af99c013a617363a1e6417629e080ab2396fb5
Garry Newman <garrynewman@users.noreply.github.com>
2021-10-02 14:22:36 +0100

    Remove obsolete, unused

--- code/ui/Scoreboard.cs
+++ code/ui/Scoreboard.cs
@@ -12,23 +12,10 @@ public class Scoreboard : Sandbox.UI.Scoreboard<ScoreboardEntry>
 		Header.Add.Label( "kills", "kills" );
 		Header.Add.Label( "deaths", "deaths" );
 		Header.Add.Label( "ping", "ping" );
-		Header.Add.Label( "fps", "fps" );
 	}
 }
 
 public class ScoreboardEntry : Sandbox.UI.ScoreboardEntry
 {
-	public Label Fps;
 
-	public ScoreboardEntry()
-	{
-		Fps = Add.Label( "", "fps" );
-	}
-
-	public override void UpdateFrom( PlayerScore.Entry entry )
-	{
-		base.UpdateFrom( entry );
-
-		Fps.Text = entry.Get<int>( "fps", 0 ).ToString();
-	}
 }
